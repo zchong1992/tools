fa049b02e354e168ad74b2d066014d9afcabeb60
zhengchong <chongzheng.zc@alibaba-inc.com>
2022-02-25 11:56:51 +0800

    添加文件夹

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tools
 常用工具箱
+
+### 文件夹中文件格式转换，批量转换utf8 
+#### file_format.py dir
---
+++ file_format.py
@@ -0,0 +1,48 @@
+
+import os
+import sys
+import codecs
+import chardet
+
+def getEncode(filename):
+    with open(filename, 'rb') as f:
+        data = f.read()
+        f_charInfo=chardet.detect(data)
+        #print (f_charInfo)
+        return f_charInfo['encoding']
+
+def convert(filename,out_enc="UTF-8"):
+    print("deal file {0}".format(filename))
+    encode=getEncode(filename)
+    try:
+        with open(filename, 'rb') as f:
+            content = f.read()
+            print(filename+" is "+encode)
+            if encode=="utf-8" or encode=="ascii":
+                print("pass")
+                return 
+            str=content.decode(encoding=encode)
+            codecs.open(filename+"bak.java",'w').write(str)
+        return 
+    except IOError as err:
+        print("I/O error:{0}".format(err))
+    
+
+def explore(dir):
+    for root,dirs,files in os.walk(dir):
+        for file in files:
+            if os.path.splitext(file)[1]=='.java':
+                print (file)
+                path=os.path.join(root,file)
+                convert(path)
+
+def main():
+    if len(sys.argv) <2:
+        print("need work dir ")
+        return 
+    print("work dir is {0}".format(sys.argv[1]))
+    explore(sys.argv[1])
+
+
+if __name__=="__main__":
+    main()
